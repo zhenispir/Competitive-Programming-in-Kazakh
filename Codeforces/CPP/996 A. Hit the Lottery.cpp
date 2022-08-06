3b0fee88fa750d7838b86b383701e489b2fc71a7
Zhenis Pirnepessov <yenispiro@gmail.com>
2022-08-06 15:25:37 +0500

    Added some new files

---
+++ Codeforces/CPP/996 A. Hit the Lottery.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, n100, n20, n10, n5, n1;
+    cin >> n;
+
+    n100 = n / 100;
+    n20 = (n - n100 * 100) / 20;
+    n10 = ((n - n100 * 100) - n20 * 20) / 10;
+    n5 = (((n - n100 * 100) - n20 * 20) - n10 * 10) / 5;
+    n1 = ((((n - n100 * 100) - n20 * 20) - n10 * 10) - n5 * 5) / 1;
+
+    cout << n100 + n20 + n10 + n5 + n1;
+    return 0;
+}
