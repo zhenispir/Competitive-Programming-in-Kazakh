25431549580ff58f7585ecf938d761fd12ea2929
Zhenis Pirnepessov <yenispiro@gmail.com>
2022-08-10 10:27:05 +0500

    1716A completely solved

--- Codeforces/Python/1716A.py
+++ Codeforces/Python/1716A.py
@@ -1,15 +1,28 @@
 '''
 Name: 2-3 Moves
 Link: https://codeforces.com/problemset/problem/1716/A
-Categories: while
+Categories: for, list, printing list
 '''
 
 t = int(input())
-i = 0
-temp
-while t > 0:
-    n = int(input())
-    if n == 0:
-        print(0)
-    elif
-    t += 1
+n = []
+result = []
+for i in range(t):
+    n.append(int(input()))
+
+for i in range(t):
+    # condition for solution
+    if abs(n[i]) == 1:
+        r = 2
+    elif abs(n[i]) == 2:
+        r = 1
+    elif abs(n[i]) % 3 == 0:
+        r = abs(n[i]) // 3
+    else:
+        r = abs(n[i])//3 + 1
+
+    # adding result to the list
+    result.append(r)
+
+# printing list in seperate lines
+print(*result, sep="\n")
