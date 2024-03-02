9e6f7fb55d1998c2005036cfd6fb553d3aba1224
Zhenis Pir <yenispiro@gmail.com>
2024-03-02 07:12:45 +0500

    Update 1934A.py

--- Codeforces/Python/1934A.py
+++ Codeforces/Python/1934A.py
@@ -1,9 +1,7 @@
 t = int(input())
-for i in range(t):
+for _ in range(t):
     n = int(input())
-    a = list(map(int, input().split()))[:n]
-    ssum=0
-    for i in range(n):
-        
-        ssum += abs(a[i]-a[i+1])
-    print(ssum)
+    a = list(map(int, input().split()))
+    max_val = max(a)
+    min_val = min(a)
+    print(max(abs(max_val - a[0]) + abs(a[0] - a[-1]) + abs(a[-1] - min_val), abs(min_val - a[0]) + abs(a[0] - a[-1]) + abs(a[-1] - max_val)))
