b904ca5df5b87d78736b00b777ce2d41837775eb
Zhenis Pir <yenispiro@gmail.com>
2024-03-14 19:26:47 +0500

    268A and 381A are added

---
+++ Codeforces/Python/381A.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = list(map(int, input().split()))
+s, d = 0, 0
+l, r = 0, n - 1
+for i in range(n):
+    if i % 2 == 0:
+        if a[l] > a[r]:
+            s += a[l]
+            l += 1
+        else:
+            s += a[r]
+            r -= 1
+    else:
+        if a[l] > a[r]:
+            d += a[l]
+            l += 1
+        else:
+            d += a[r]
+            r -= 1
