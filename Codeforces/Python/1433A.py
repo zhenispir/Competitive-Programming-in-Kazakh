d8b226b00ef4c5cd4810f7b26179e712bb6e3346
Zhenis Pir <yenispiro@gmail.com>
2024-03-24 14:23:43 +0500

    Create 1433A.py

---
+++ Codeforces/Python/1433A.py
@@ -0,0 +1,11 @@
+t = int(input())
+for i in range(t):
+    n = (input())
+    if len(n) == 1:
+        print(n)
+    elif len(n) == 2:
+        print(9+int(n[0])*2)
+    elif len(n) == 3:
+        print(9+18+int(n[0])*3)
+    else:  
+        print(9+18+27+int(n[0])*4)
