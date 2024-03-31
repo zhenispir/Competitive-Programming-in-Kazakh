891d4e3f1c0ad74a9e10b61c237a351a8e865f06
Zhenis Pir <yenispiro@gmail.com>
2024-03-31 12:02:11 +0500

    707A's second solution is added. More simpler version I think

---
+++ Codeforces/Python/707A-2.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+color = '#Black&White'
+for i in range(n):
+    x = input()
+    if ('Y' in x or 'C' in x or 'M' in x) and color != '#Color':
+        color = '#Color'
+print(color)
