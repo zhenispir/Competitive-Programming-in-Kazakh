f2b6cec8ba24199a017f1a2434921d6038a8b751
Zhenis Pir <yenispiro@gmail.com>
2024-04-01 09:49:32 +0500

    Update 707A-2.py

--- Codeforces/Python/707A-2.py
+++ Codeforces/Python/707A-2.py
@@ -3,6 +3,6 @@ n, m = map(int, input().split())
 color = '#Black&White'
 for i in range(n):
     x = input()
-    if ('Y' in x or 'C' in x or 'M' in x) and color != '#Color':
+    if ('Y' in x or 'C' in x or 'M' in x):
         color = '#Color'
 print(color)
