7bdbd0d157b112502ac2da8ddb716ad89eadf260
Zhenis Pir <yenispiro@gmail.com>
2024-08-06 20:24:40 +0500

    1999B is tried

--- Codeforces/Python/1999B.py
+++ Codeforces/Python/1999B.py
@@ -1,45 +1,34 @@
 def count_wins(a1, a2, b1, b2):
-    # Possible flips for Suneet: (a1, a2), (a1, b2), (a2, b1), (a2, b2)
-    # Possible flips for Slavic: (b1, b2), (b1, a2), (b2, a1), (b2, a2)
-    suneet_wins = 0
-    games = [
-        ((a1, b1), (a2, b2)),
-        ((a1, b2), (a2, b1)),
-        ((a2, b1), (a1, b2)),
-        ((a2, b2), (a1, b1)),
-        ((a1, b1), (b2, a2)),
-        ((a1, b2), (b1, a2)),
-        ((a2, b1), (b2, a1)),
-        ((a2, b2), (b1, a1))
-    ]
+    suneet_cards = [a1, a2]
+    slavic_cards = [b1, b2]
     
-    for game in games:
-        (s1, s2), (s3, s4) = game
-        suneet_score = 0
-        slavic_score = 0
-        
-        # First round
-        if s1 > s2:
-            suneet_score += 1
-        elif s1 < s2:
-            slavic_score += 1
-        
-        # Second round
-        if s3 > s4:
-            suneet_score += 1
-        elif s3 < s4:
-            slavic_score += 1
-        
-        # Check if Suneet wins
-        if suneet_score > slavic_score:
-            suneet_wins += 1
+    suneet_wins = 0
+    for i in range(2):
+        for j in range(2):
+            suneet_score = 0
+            slavic_score = 0
+            
+            # First round
+            if suneet_cards[i] > slavic_cards[j]:
+                suneet_score += 1
+            elif suneet_cards[i] < slavic_cards[j]:
+                slavic_score += 1
+            
+            # Second round
+            if suneet_cards[1-i] > slavic_cards[1-j]:
+                suneet_score += 1
+            elif suneet_cards[1-i] < slavic_cards[1-j]:
+                slavic_score += 1
+            
+            # Check if Suneet wins more rounds
+            if suneet_score > slavic_score:
+                suneet_wins += 1
     
     return suneet_wins
 
-# Read number of test cases
+
 t = int(input())
 
-# Process each test case
 for _ in range(t):
     a1, a2, b1, b2 = map(int, input().split())
     print(count_wins(a1, a2, b1, b2))
